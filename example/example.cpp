1ca6ad200c20bcbf9467b6cd36716a097864bd4a
sclaiborne <scott@loupe.team>
2021-11-01 13:15:15 -0700

    Add example code

---
+++ example/example.cpp
@@ -0,0 +1,21 @@
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "../LLHttpH.h"
+#include "../HttpUtility.h"
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+int main(int argc, char const *argv[]) {
+	printf("This is an example");
+	/* code */
+	return 0;
+}
