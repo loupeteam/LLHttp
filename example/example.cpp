e78d1cf28f9191aad27662608acb69ea63c82908
sclaiborne <scott@loupe.team>
2021-11-01 13:27:08 -0700

    Add some code to example.cpp

--- example/example.cpp
+++ example/example.cpp
@@ -16,6 +16,13 @@ extern "C" {
 using namespace std;
 int main(int argc, char const *argv[]) {
 	printf("This is an example");
+
+	HttpParse_typ parser = {};
+
+	parser.data = (UDINT)&"HTTP/1.0 200 OK\r\ncustom-header: 1\r\n\r\n";
+	parser.dataLength = strlen((char*)parser.data);
+	HttpParse(&parser);
+
 	/* code */
 	return 0;
 }
